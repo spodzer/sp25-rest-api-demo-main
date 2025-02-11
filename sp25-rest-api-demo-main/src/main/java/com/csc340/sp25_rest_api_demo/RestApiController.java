4e0c3fc1643adb82e91099a1ef27d32c16342ce9
spodzer <laquancrawford031@gmail.com>
2025-02-11 11:03:11 -0500

    1

--- sp25-rest-api-demo-main/src/main/java/com/csc340/sp25_rest_api_demo/RestApiController.java
+++ sp25-rest-api-demo-main/src/main/java/com/csc340/sp25_rest_api_demo/RestApiController.java
@@ -6,11 +6,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -20,8 +16,6 @@ public class RestApiController {
 
     /**
      * Hello World API endpoint.
-     *
-     * @return response string.
      */
     @GetMapping("/hello")
     public String hello() {
@@ -30,20 +24,14 @@ public class RestApiController {
 
     /**
      * Greeting API endpoint.
-     *
-     * @param name the request parameter
-     * @return the response string.
      */
     @GetMapping("/greeting")
     public String greeting(@RequestParam(value = "name", defaultValue = "Dora") String name) {
         return "Hola, soy " + name;
     }
 
-
     /**
      * List all students.
-     *
-     * @return the list of students.
      */
     @GetMapping("students/all")
     public Object getAllStudents() {
@@ -54,22 +42,15 @@ public class RestApiController {
     }
 
     /**
-     * Get one student by Id
-     *
-     * @param id the unique student id.
-     * @return the student.
+     * Get one student by Id.
      */
     @GetMapping("students/{id}")
     public Student getStudentById(@PathVariable int id) {
         return studentDatabase.get(id);
     }
 
-
     /**
      * Create a new Student entry.
-     *
-     * @param student the new Student
-     * @return the List of Students.
      */
     @PostMapping("students/create")
     public Object createStudent(@RequestBody Student student) {
@@ -78,10 +59,7 @@ public class RestApiController {
     }
 
     /**
-     * Delete a Student by id
-     *
-     * @param id the id of student to be deleted.
-     * @return the List of Students.
+     * Delete a Student by id.
      */
     @DeleteMapping("students/delete/{id}")
     public Object deleteStudent(@PathVariable int id) {
@@ -90,16 +68,11 @@ public class RestApiController {
     }
 
     /**
-     * Get a list of universities from hipolabs and make them available at our own API
-     * endpoint.
-     *
-     * @return json array
+     * Get a list of universities from Hipolabs API.
      */
     @GetMapping("/univ")
     public Object getUniversities() {
         try {
-            
-            //CONSUMING A RESTFUL WEB SERVICE (API)
             String url = "http://universities.hipolabs.com/search?name=sports";
             RestTemplate restTemplate = new RestTemplate();
             ObjectMapper mapper = new ObjectMapper();
@@ -107,9 +80,7 @@ public class RestApiController {
             String jsonListResponse = restTemplate.getForObject(url, String.class);
             JsonNode root = mapper.readTree(jsonListResponse);
 
-            //The response from the above API is a JSON Array, which we loop through.
             for (JsonNode rt : root) {
-                //Extract relevant info from the response and use it for what you want, in this case just print to the console.
                 String name = rt.get("name").asText();
                 String country = rt.get("country").asText();
                 System.out.println(name + ": " + country);
@@ -117,22 +88,17 @@ public class RestApiController {
 
             return root;
         } catch (JsonProcessingException ex) {
-            Logger.getLogger(RestApiController.class.getName()).log(Level.SEVERE,
-                    null, ex);
+            Logger.getLogger(RestApiController.class.getName()).log(Level.SEVERE, null, ex);
             return "error in /univ";
         }
-
     }
-    
+
     /**
-     * Get a list of 15 GSO breweries and make them available at our own API endpoint.
-     *
-     * @return a list of Breweries.
+     * Get a list of 15 GSO breweries from OpenBreweryDB API.
      */
     @GetMapping("/brew")
     public Object getBreweries() {
         try {
-            //CONSUMING A RESTFUL WEB SERVICE (API)
             String url = "https://api.openbrewerydb.org/v1/breweries?by_city=greensboro";
             RestTemplate restTemplate = new RestTemplate();
             ObjectMapper mapper = new ObjectMapper();
@@ -142,11 +108,9 @@ public class RestApiController {
 
             List<Brewery> breweryList = new ArrayList<>();
 
-            //The response from the above API is a JSON Array, which we loop through.
             for (JsonNode rt : root) {
-                //Extract relevant info from the response and use it for what you want, in this case build a Brewery object
                 String name = rt.get("name").asText();
-                String address = rt.get("address_1").asText();
+                String address = rt.has("address_1") ? rt.get("address_1").asText() : "N/A";
                 String type = rt.get("brewery_type").asText();
 
                 Brewery brewery = new Brewery(name, address, type);
@@ -154,57 +118,44 @@ public class RestApiController {
             }
             return breweryList;
         } catch (JsonProcessingException ex) {
-            Logger.getLogger(RestApiController.class.getName()).log(Level.SEVERE,
-                    null, ex);
+            Logger.getLogger(RestApiController.class.getName()).log(Level.SEVERE, null, ex);
             return "error in /brew";
         }
     }
 
     /**
-     * Get info about a fruit from fruityVice.
-     *
-     * @param fruitName
-     * @return a Fruit object.
+     * Get info about a fruit from FruityVice API.
      */
     @GetMapping("/fruit")
     public Object getFruit(@RequestParam(value = "name", defaultValue = "kiwi") String fruitName) {
         try {
-            //CONSUMING A RESTFUL WEB SERVICE (API)
             String url = "https://www.fruityvice.com/api/fruit/" + fruitName;
             RestTemplate restTemplate = new RestTemplate();
             ObjectMapper mapper = new ObjectMapper();
 
-            /**
-             * The response from the above API is a 'single' JSON object that looks like this:
-             * <pre>
-             *     {
-             *     "name": "Kiwi",
-             *     "id": 66,
-             *     "family": "Actinidiaceae",
-             *     "order": "Struthioniformes",
-             *     "genus": "Apteryx",
-             *     "nutritions": {
-             *         "calories": 61,
-             *         "fat": 0.5,
-             *         "sugar": 9.0,
-             *         "carbohydrates": 15.0,
-             *         "protein": 1.1
-             *     }
-             * }
-             * </pre>
-             * I know this because I TESTED THE FRUITYVICE API IN POSTMAN!!!
-             */
             String jsonListResponse = restTemplate.getForObject(url, String.class);
             JsonNode root = mapper.readTree(jsonListResponse);
 
-            //Extract relevant info from the response and use it for what you want, in this case build a Fruit object
             Fruit fruit = new Fruit(root.get("name").asText(), root.get("family").asText(),
                     root.get("nutritions").get("calories").asDouble());
+
             return fruit;
         } catch (JsonProcessingException ex) {
-            Logger.getLogger(RestApiController.class.getName()).log(Level.SEVERE,
-                    null, ex);
+            Logger.getLogger(RestApiController.class.getName()).log(Level.SEVERE, null, ex);
             return "error in /fruit";
         }
+    }
+}
+
+@RestController
+@RequestMapping("/dogs")
+class DogController {
+
+    @GetMapping
+    public List<Dog> getAllDogs() {
+        return Arrays.asList(
+                new Dog(1, "Labrador Retriever", "Companion", "10-12 years", "Friendly, Energetic",,
+                new Dog(2, "German Shepherd", "Herding", "9-13 years", "Intelligent, Loyal",)
+        );
     }
 }
